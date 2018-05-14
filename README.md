c933364a7cff7b3161a4a3e6c63886b13cdd5db6
Thomas Miedema <thomasmiedema@gmail.com>
2018-05-14 16:45:54 +0200

    Add client.py

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ pip install -r requirements.txt
 FLASK_DEBUG=1 FLASK_APP=hello.py flask run
 ```
 
-# Client
+# Client (client credentials authorization grant)
 
+First edit OAuthClient parameters in client.py. Then run
 ```
-curl --header 'Authorization: Bearer dca6f1125fb4874e058887a098dadf91f0e1e70d' 'http://127.0.0.1:5000/'
+$ python client.py
 ```
---
+++ client.py
@@ -0,0 +1,33 @@
+from authlib.client import OAuthClient
+import requests
+
+
+# TODO: update
+client = OAuthClient(
+    client_id='',
+    client_secret='',
+    api_base_url='https://api.github.com/',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+    client_kwargs={'scope': 'user:email'},
+)
+
+
+# TODO: uncomment
+#token = client.fetch_access_token(grant_type='client_credentials')
+#print(token)
+
+
+# TODO: remove
+token = {
+    'access_token': 'dca6f1125fb4874e058887a098dadf91f0e1e70d',
+    'token_type': 'bearer',
+    'scope': 'user:email'
+}
+
+
+r = requests.get('http://127.0.0.1:5000/', headers={'Authorization': 'Bearer {access_token}'.format(**token)})
+print()
+print(r.text)
+print()
+r.raise_for_status()
